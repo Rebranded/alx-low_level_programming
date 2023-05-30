12198687e674d10eefba96447a9c5a0ea703a404
Rebranded <akwaowombo@gmail.com>
2023-05-29 21:19:58 -0700

    prints commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		printf("%d", b);
+	}
+	putchar('\n');
+	return (0);
+}
